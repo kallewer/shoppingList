050096941af276ee676d60a1f9a83d2232596e27
Karl Werner <karlmoritz.werner@gmail.com>
2023-01-30 17:53:06 +0100

    Added filewriter and output .txt file

--- src/FoodType.java
+++ src/FoodType.java
@@ -6,6 +6,7 @@ public enum FoodType {
     FISH,
     PANTRY,
     SPICES,
+    OILS,
     BREAD,
     FROZEN,
     BEVERAGES,
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
 
         ShoppingList list = new ShoppingList();
         list.add(
-                ShoppingListEntry.of(6, "Eier", FoodType.PANTRY),
-                ShoppingListEntry.of(1, Unit.l, "Hafermilch", FoodType.DAIRY),
-                ShoppingListEntry.of(500, Unit.g, "Mehl", FoodType.PANTRY),
-                ShoppingListEntry.of(1, Unit.pack, "Bananen", FoodType.FRUITS),
-                ShoppingListEntry.of(1, Unit.glass, "Apfelmus",FoodType.PANTRY)
+                ShoppingListEntry.of(240, Unit.ml, "Weißer Balsamico", FoodType.OILS),
+                ShoppingListEntry.of(70, Unit.g, "Gelbe Senfsaat", FoodType.SPICES),
+                ShoppingListEntry.of(1, Unit.pack, "Zucker", FoodType.PANTRY),
+                ShoppingListEntry.of(1, Unit.pack, "Senfblaetter", FoodType.SPICES),
+                ShoppingListEntry.of(1, Unit.pack, "Senfsaat", FoodType.SPICES),
+                ShoppingListEntry.of(1, Unit.pack, "Piment", FoodType.SPICES),
+                ShoppingListEntry.of(1, Unit.pack, "Weißer Pfeffer", FoodType.SPICES),
+                ShoppingListEntry.of(1, Unit.pack, "Schwarzer Pfeffer", FoodType.SPICES),
+                ShoppingListEntry.of(1, Unit.pack, "Koriandersaat", FoodType.SPICES),
+                ShoppingListEntry.of(300, Unit.ml, "Weißwein", FoodType.BEVERAGES),
+                ShoppingListEntry.of(240, Unit.ml, "Weißer Balsamico", FoodType.OILS),
+                ShoppingListEntry.of(200, Unit.g, "Silberzwiebeln", FoodType.VEGETABLES)
         );
-        System.out.println(list.printOnly(e -> e.getCategory() == FoodType.PANTRY));
-        list.add(ShoppingListEntry.of(1000, Unit.g,"mehl", FoodType.PANTRY));
-        System.out.println(list.print());
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("shoppingList.txt"));
+            writer.write(list.print());
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 }
